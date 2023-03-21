c642d10731649d3bdc39938ecd3d13b65f80bec1
Hsu Myat Moe <hsumyatm7308@gmail.com>
2023-03-21 07:29:24 +0630

    21-3-23

--- part1/l14arraymethod.php
+++ part1/l14arraymethod.php
@@ -11,7 +11,27 @@ echo "<pre>".print_r(array_chunk($couples,2,true),"true")."</pre>";
 echo "<pre>".print_r(array_chunk($couples,2,false),"true")."</pre>";
 
 
+
+// array_combine() Function 
+                //  key , value 
+// array_combine(array1,array2) Function 
+
+$name = array("aung aung","su su","maung maung","hla hla");
+$age = ["30","25","35","40"];
+
+echo "<pre>".print_r(array_combine($name,$age),"true")."</pre>";
+echo array_combine($name,$age)["aung aung"]; //30
+
+echo "<hr>";
+
+// $car = ["toyota","suzuki","mazada","force"];
+$car = ["toyota","suzuki","mazada","force","force","suzuki"];
+echo "<pre>".print_r(array_count_values($car),"true")."</pre>";
+echo "<pre>".print_r(count($car),"true")."</pre>";
+
+
+
+
 ?>
 
 
-<!-- 17AM  -->
