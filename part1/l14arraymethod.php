d678dd9bfe51c2af5afdff60bf20e227f22122e6
Hsu Myat Moe <hsumyatm7308@gmail.com>
2023-03-21 07:57:25 +0630

    21-3-13

--- part1/l14arraymethod.php
+++ part1/l14arraymethod.php
@@ -30,6 +30,46 @@ echo "<pre>".print_r(array_count_values($car),"true")."</pre>";
 echo "<pre>".print_r(count($car),"true")."</pre>";
 
 
+echo "<hr>";
+
+// array_diff() Function 
+// array_diff(array1,array2,array3,...) 
+// Note:we don't need to consider any index of keyname' 
+
+$colors1 = ["red","green","blue","pink"];
+$colors2 = ["red","blue","pink","silver"];
+$colors3 = ["green","blue","pink","violet"];
+echo "<pre>".print_r(array_diff($colors1,$colors2),"true")."</pre>"; //green 
+echo "<pre>".print_r(array_diff($colors2,$colors1),"true")."</pre>"; //silver
+echo "<pre>".print_r(array_diff($colors1,$colors2,$colors3),"true")."</pre>"; //no 
+
+ 
+$col1 = array("a" => "red","b"=>"green","c"=>"blue","d"=>"yellow");
+$col2 = array("e"=>"red","f"=>"green","g"=>"black");
+echo "<pre>".print_r(array_diff($col1,$col2),"true")."</pre>"; //blue yellow  
+echo "<pre>".print_r(array_diff($col2,$col1),"true")."</pre>"; //black  
+
+echo "<hr>";
+
+// array_diff_assoc() Function 
+// array_diff_assoc(array1,array2,array3,...) 
+$col1 = array("a" => "red","b"=>"yellow","c"=>"blue","d"=>"green"); 
+$col2 = array("a" => "red","b"=>"green","c"=>"blue"); 
+
+echo "<pre>".print_r(array_diff_assoc($col1,$col2),"true")."</pre>"; //yellow green   
+echo "<pre>".print_r(array_diff_assoc($col2,$col1),"true")."</pre>"; //green 
+
+echo "<hr>";
+
+// array_diff_key() 
+$col1 = array("a" => "red","b"=>"yellow","c"=>"blue","d"=>"green","f"=>"pink"); 
+$col2 = array("a" => "red","b"=>"green","c"=>"blue","e"=>"orange"); 
+
+echo "<pre>".print_r(array_diff_key($col1,$col2),"true")."</pre>"; //gray pink   
+echo "<pre>".print_r(array_diff_key($col2,$col1),"true")."</pre>"; //orange 
+
+
+
 
 
 ?>
