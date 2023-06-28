2098a22d7a72b8a9cafb042d88f57473a34020b6
JV <107622230+hsumyatm7308@users.noreply.github.com>
2023-06-28 16:41:10 +0630

    27 june

---
+++ l54stepformverification/userlist.php
@@ -0,0 +1,98 @@
+<?php
+
+
+ini_set("display_errors",1);
+session_start();
+require_once "./dbconnect.php";
+
+
+if(!isset($_SESSION['email']) && !isset($_SESSION['password'])){
+    header('Location:./signup.php');
+ }
+ 
+
+try {
+
+    $stmt = $conn->prepare("SELECT id,firstname,lastname,email,dob,phone,address FROM users");
+    $stmt->execute();
+} catch (PDOException $e) {
+    echo "Error Found: " . $e->getMessage();
+}
+
+$conn = null;
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>User List Form</title>
+    <link href="./style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+    <div class="container">
+
+        <div class="dotbox">
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+        </div>
+
+
+
+
+        <div id="userlist-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Birthday</th>
+                        <th>Phone</th>
+                        <th>Address</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <?php while($row = $stmt->fetch()): ?>
+                    <tr>
+                        <td><?php echo "{$row['id']}" ?> </td>
+                        <td><?php echo "{$row['firstname']} {$row['lastname']}" ?> </td>
+                        <td><?php echo "{$row['email']}" ?> </td>
+                        <td><?= "{$row['dob']}" ?> </td>
+                        <td><?=  "{$row['phone']}" ?> </td>
+                        <td><?=  "{$row['address']}" ?> </td>
+                    </tr>
+                    <?php endwhile; ?>
+
+
+                </tbody>
+
+
+            </table>
+
+            <button type="submit" class="submitbtn" onclick="goregister()">Create User</button>
+        </div>
+
+    </div>
+
+
+
+    <script type="text/javascript">
+        function goregister(){
+            window.location.href = "./index.php";
+        }
+        
+    </script>
+</body>
+
+</html>
---
+++ l55cookiesset.php
@@ -0,0 +1,23 @@
+<?php 
+
+// =>Syntax (Note :: gone after browser)
+// setcookie('cookiename','value');
+
+// setcookie('mmk','Myanmar Kyat');
+// echo "Cookies Set Successfully";
+
+
+
+// =>Syntax (Note :: gone after browser)
+// setcookie('cookiename','value','expire');
+
+setcookie('mmk','Myanmar Kyat',time()+60*60*24*3);
+echo "Cookies Set Successfully";
+
+
+
+
+
+
+
+?>
